0369655ba5abe3dbaaf48449823c5c5d6bd39c0b
Jason Zhang <turtle_shell@yeah.net>
2016-07-26 16:22:19 +0800

    meow

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/public/index.html');
 });
 
-http.listen(3000);
+// http.listen(3000);
 app.use(express.static(__dirname + '/public/images'));
 app.use(express.static(__dirname + '/public/styles'));
 app.use(express.static(__dirname + '/public/scripts'));
